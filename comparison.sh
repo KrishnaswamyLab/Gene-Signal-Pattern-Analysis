8c88a39a31f6cae12c976ec9122dacb86ec83f0f
Aarthi Venkat <av622@login2.mccleary.ycrc.yale.edu>
2023-10-30 21:44:08 -0400

    overview figure

--- comparison.sh
+++
@@ -1,8 +0,0 @@
-for model in GAE_att_Ggene Signals GSPA GSPA_QR MAGIC Node2Vec_Gcell DiffusionEMD GAE_noatt_Gcell GAE_att_Gcell Node2Vec_Ggene GAE_noatt_Ggene; do
-    echo ${model}
-    for task in localization coexpression; do
-        for run in {0..2}; do
-            python train.py --model ${model} --task ${task} --save-as ${run}
-	done
-    done
-done
